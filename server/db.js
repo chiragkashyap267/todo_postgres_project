d0ced6c6b68334bde97b7c44ff3234bfd4150216
chirag <businesswithchirag267@gmail.com>
2026-09-19 17:44:57 +0530

    fix NODE_ENV casing for ssl config

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const {Pool} = pg;
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl : process.env.Node_ENV === 'production'
+    ssl : process.env.NODE_ENV === 'production'
     ? {rejectUnauthorized : false }
     : false,
 })
